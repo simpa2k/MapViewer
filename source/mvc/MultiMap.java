428696dc34b6b60918ba0dc9bd839828dc1ee233
Simon Olofsson <s.olofsson@gmail.com>
2016-04-04 19:22:39 +0200

    Now tracking MultiMap.java.

---
+++ source/mvc/MultiMap.java
@@ -0,0 +1,78 @@
+package mvc;
+
+import places.*;
+import java.util.*;
+
+public class MultiMap<K, V> {
+
+	HashMap<K, ArrayList<V>> map;
+
+	public MultiMap() {
+
+		map = new HashMap<K, ArrayList<V>>();
+
+	}
+
+	public void put(K key, V value) {
+
+		ArrayList<V> values = map.get(key);
+
+		if(values == null) {
+
+			values = new ArrayList<V>();
+			map.put(key, values);
+
+		}
+		values.add(value);
+
+	}
+
+	public ArrayList<V> get(K key) {
+
+		ArrayList<V> values = map.get(key);
+
+		return values;
+
+	}
+
+	public void remove(K key, V value) {
+
+		ArrayList<V> values = map.get(key);
+
+		values.remove(value);
+
+	}
+
+	public void removeIf(Remover remover) {
+
+		Iterator<K> mapIterator = map.keySet().iterator();
+
+		while(mapIterator.hasNext()) {
+
+			ArrayList<V> values = map.get(mapIterator.next());
+			
+			Iterator<V> listIterator = values.iterator();
+
+			while(listIterator.hasNext()) {
+
+				V nextItem = listIterator.next();
+
+				if(remover.remove(nextItem)) {
+
+					listIterator.remove();
+
+				}
+
+			}
+		}
+
+	}
+
+	@FunctionalInterface
+	interface Remover<K> {
+
+		public boolean remove(K key);
+
+	}
+
+}
